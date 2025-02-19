17848c5d58716c38905131546cef571af4b7a23e
Hyojoong,Kim <115703110+hn7093@users.noreply.github.com>
2025-02-19 20:50:12 +0900

    feat : 블록 생명 추가 및 자동 생성 구현

--- Assets/03.Scripts/MiniGame1/Block.cs
+++ Assets/03.Scripts/MiniGame1/Block.cs
@@ -4,13 +4,46 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
+    public int maxLife = 3;
+    public int life = 3;
+
+    [SerializeField] private SpriteRenderer _spriteRenderer;
     private void OnCollisionExit2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Ball"))
         {
-            // 점수 증가
-            BlockScoreManager.Instance.AddScore(1);
-            Destroy(gameObject);
+            life--;
+            if (life <= 0)
+            {
+                // 점수 증가
+                BlockScoreManager.Instance.AddScore(1);
+                BlockGameManager.Instance.RemoveBlock(this);
+                Destroy(gameObject);
+            }
+            else
+            {
+                // 투명도 감소
+                Color color = _spriteRenderer.color;
+                color.a = (float)life/maxLife;
+                _spriteRenderer.color = color;
+            }
+
         }
+    }
+    public void SetHp(int maxHp)
+    {
+        maxLife = maxHp;
+        life = maxLife;
+
+        // 생명 2부터는 색깔
+        if(maxHp > 1)
+            _spriteRenderer.color = GetRandomColor();
+    }
+    Color GetRandomColor()
+    {
+        float r = Random.Range(100, 250) / 255.0f;
+        float g = Random.Range(100, 250) / 255.0f;
+        float b = Random.Range(100, 250) / 255.0f;
+        return new Color(r, g, b);
     }
 }
--- Assets/03.Scripts/MiniGame1/BlockGameManager.cs
+++ Assets/03.Scripts/MiniGame1/BlockGameManager.cs
@@ -7,9 +7,13 @@ public class BlockGameManager : MonoBehaviour
     static BlockGameManager instance;
     public static BlockGameManager Instance { get { return instance; } }
     [SerializeField] private GameObject ballPrefab;
+    [SerializeField] private GameObject blockPrefab;
     public GameState gameState = GameState.Ready;
     public static bool isFirstLoading = true;
+    private int blockCount = 20;
+    private List<Block> blocks;
     private PlayerBlock player;
+    private int level = 1;
     private void Awake()
     {
         if (instance == null)
@@ -21,14 +25,17 @@ public class BlockGameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        blocks = new List<Block>(blockCount);
         player = FindObjectOfType<PlayerBlock>();
         if (!isFirstLoading)
         {
             StartGame();
+            SpawnBlock();
         }
         else
         {
             isFirstLoading = false;
+            SpawnBlock();
         }
     }
 
@@ -36,6 +43,33 @@ public class BlockGameManager : MonoBehaviour
     void Update()
     {
 
+    }
+    private void SpawnBlock()
+    {
+        
+        for (int i = 0; i < blockCount; i++)
+        {
+            float x = (i % 5) * 1.6f - 3.2f;
+            float y = (i / 5) * 1.0f;
+
+            Block block = Instantiate(blockPrefab).GetComponent<Block>();
+            block.transform.position = new Vector3(x, y, 0);
+            blocks.Add(block);
+            // 레벨별로 최대 HP를 랜덤 설정
+            int hp = Random.Range(1, level+1);
+            Debug.Log(hp);
+            blocks[i].SetHp(hp);
+        }
+    }
+    public void RemoveBlock(Block block)
+    {
+        blocks.Remove(block);
+        // 남은게 없으면 다음 레벨로 생성
+        if(blocks.Count == 0)
+        {
+            level++;
+            SpawnBlock();
+        }
     }
     public void ChangeState(GameState gameState)
     {
@@ -45,9 +79,8 @@ public class BlockGameManager : MonoBehaviour
     {
         ChangeState(GameState.Playing);
         BlockScoreManager.Instance.ActiveUI(gameState);
-        if(player == null)
+        if (player == null)
         {
-            Debug.Log("player null");
             player = FindObjectOfType<PlayerBlock>();
         }
         // 시작 볼 생성
