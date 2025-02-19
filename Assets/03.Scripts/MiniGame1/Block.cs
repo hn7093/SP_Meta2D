4b2e9e4e7256b6544d6039c760377d94a35ec6ab
Hyojoong,Kim <115703110+hn7093@users.noreply.github.com>
2025-02-19 19:17:16 +0900

    feat : 오브젝트 반사 구현

--- Assets/03.Scripts/MiniGame1/Block.cs
+++ Assets/03.Scripts/MiniGame1/Block.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionExit2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Ball"))
         {
